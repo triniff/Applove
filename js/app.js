c3e1f0f4675b99158b57c08a672fb642383488df
Trinidad Fuentes <tfuentesfuentealba@gmail.com>
2017-05-31 16:02:46 -0400

    creando galeria

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+ (function galeria(){
+ 	var contenedor = document.getElementById("work");
+ 	var caja = Array.from(document.getElementsByClassName("col-4"));
+ 	var nuevoDiv, imagen, cerrar;
+ 	caja.forEach(function(c){
+ 		c.addEventListener("click", function(){
+ 			nuevoDiv = document.createElement("div");
+ 			nuevoDiv.classList.add("galeria");
+
+ 			cerrar = document.createElement("i");
+ 			cerrar.classList.add("fa", "fa-times-circle-o");
+
+ 			nuevoDiv.appendChild(cerrar);
+ 			nuevoDiv.innerHTML = c.innerHTML;
+ 			contenedor.appendChild(nuevoDiv);
+
+ 		})
+ 	})
+
+ })()
